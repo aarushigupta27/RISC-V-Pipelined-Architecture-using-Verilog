845aa5653bb49a3a37ee32aa29b507686169cc06
Aarushi Gupta <106400409+aarushigupta27@users.noreply.github.com>
2024-05-12 11:07:10 +0530

    Update

--- alu.v
+++ alu.v
@@ -1,26 +1,18 @@
-module ALU(A,B,Result,ALUControl,OverFlow,Carry,Zero,Negative);
-
-    input [31:0]A,B;
-    input [2:0]ALUControl;
-    output Carry,OverFlow,Zero,Negative;
-    output [31:0]Result;
-
-    wire Cout;
-    wire [31:0]Sum;
-
-    assign Sum = (ALUControl[0] == 1'b0) ? A + B :
-                                          (A + ((~B)+1)) ;
-    assign {Cout,Result} = (ALUControl == 3'b000) ? Sum :
-                           (ALUControl == 3'b001) ? Sum :
-                           (ALUControl == 3'b010) ? A & B :
-                           (ALUControl == 3'b011) ? A | B :
-                           (ALUControl == 3'b101) ? {{32{1'b0}},(Sum[31])} :
+module ALU(input [31:0] A,B,input [2:0] alu_control,output OverFlow,Carry,Zero,Negative, output [31:0]result);
+    wire cout;
+    wire [31:0]sum;
+    assign sum=(alu_control[0]==1'b0)?A+B:(A+((~B)+1)) ;
+    assign {cout,result} = (alu_control==3'b000)?sum :
+                           (alu_control==3'b001)?sum :
+                           (alu_control==3'b010)?A&B :
+                           (alu_control==3'b011)?A|B :
+                           (alu_control==3'b101)?{{32{1'b0}},(sum[31])} :
                            {33{1'b0}};
-    assign OverFlow = ((Sum[31] ^ A[31]) & 
-                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
-                      (~ALUControl[1]));
-    assign Carry = ((~ALUControl[1]) & Cout);
-    assign Zero = &(~Result);
-    assign Negative = Result[31];
+    assign OverFlow = ((sum[31]^A[31]) & 
+                      (~(alu_control[0]^B[31]^A[31])) &
+                      (~alu_control[1]));
+    assign Carry=((~alu_control[1]) & cout);
+    assign Zero=&(~result);
+    assign Negative=result[31];
 
 endmodule
