cc3f0cffbc3c19e3734ab8c8124d599591c33a46
Aarushi Gupta <106400409+aarushigupta27@users.noreply.github.com>
2024-05-12 11:11:06 +0530

    Update

--- alu.v
+++ alu.v
@@ -1,4 +1,4 @@
-module ALU(input [31:0] A,B,input [2:0] alu_control,output OverFlow,Carry,Zero,Negative, output [31:0]result);
+module ALU(input [31:0] A,B,input [2:0] alu_control,output overflow,carry,zero,negative, output [31:0]result);
     wire cout;
     wire [31:0]sum;
     assign sum=(alu_control[0]==1'b0)?A+B:(A+((~B)+1)) ;
@@ -8,11 +8,11 @@ module ALU(input [31:0] A,B,input [2:0] alu_control,output OverFlow,Carry,Zero,N
                            (alu_control==3'b011)?A|B :
                            (alu_control==3'b101)?{{32{1'b0}},(sum[31])} :
                            {33{1'b0}};
-    assign OverFlow = ((sum[31]^A[31]) & 
+    assign overflow = ((sum[31]^A[31]) & 
                       (~(alu_control[0]^B[31]^A[31])) &
                       (~alu_control[1]));
-    assign Carry=((~alu_control[1]) & cout);
-    assign Zero=&(~result);
-    assign Negative=result[31];
+    assign carry=((~alu_control[1]) & cout);
+    assign zero=&(~result);
+    assign negative=result[31];
 
 endmodule
